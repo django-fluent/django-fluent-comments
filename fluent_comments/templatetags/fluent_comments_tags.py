078743983bcb5de3ed9ac2d0a214bfa3f22e6e02
Diederik van der Boor <vdboor@edoburu.nl>
2012-05-25 16:24:17 +0200

    Minor docs update in templatetags/fluent_comments_tags

--- fluent_comments/templatetags/fluent_comments_tags.py
+++ fluent_comments/templatetags/fluent_comments_tags.py
@@ -5,6 +5,9 @@ register = Library()
 
 @register.inclusion_tag("fluent_comments/templatetags/ajax_comment_tags.html", takes_context=True)
 def ajax_comment_tags(context):
+    """
+    Display the required ``<div>`` elements to let the Ajax comment functionality work with your form.
+    """
     request = context['request']
 
     new_context = {}
