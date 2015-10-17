087909480561dc77b706eba05d09ec90d7eff6aa
Diederik van der Boor <vdboor@edoburu.nl>
2015-10-17 13:59:49 +0200

    Fix Django 1.9 issue with loading models in __init__.py
    This also removes the unnecessary threadedcomments import, because both
    `get_model()` and `get_form()` are already defined here. That import no
    longer works now that django-threadedcomments 1.0.1 has a similar fix
    for Django 1.9

--- fluent_comments/__init__.py
+++ fluent_comments/__init__.py
@@ -2,18 +2,9 @@
 API for :ref:`custom-comment-app-api`
 """
 from fluent_comments import appsettings
-from fluent_comments.models import FluentComment
-from fluent_comments.forms import FluentCommentForm
-
 
 # following PEP 440
 __version__ = "1.0.4"
-
-
-if appsettings.USE_THREADEDCOMMENTS:
-    # Extend the API provided by django-threadedcomments,
-    # in case this app uses more hooks of Django's custom comment app API.
-    from threadedcomments import *
 
 
 def get_model():
@@ -21,9 +12,11 @@ def get_model():
     Return the model to use for commenting.
     """
     if appsettings.USE_THREADEDCOMMENTS:
+        from threadedcomments.models import ThreadedComment
         return ThreadedComment
     else:
         # Our proxy model that performs select_related('user') for the comments
+        from fluent_comments.models import FluentComment
         return FluentComment
 
 
@@ -31,4 +24,5 @@ def get_form():
     """
     Return the form to use for commenting.
     """
+    from fluent_comments.forms import FluentCommentForm
     return FluentCommentForm
