d389588dc7da2cfd91e8a56235d09fac7759a0b6
Diederik van der Boor <vdboor@edoburu.nl>
2015-10-17 14:01:19 +0200

    setup.py: improve .mo file compiling

--- setup.py
+++ setup.py
@@ -10,10 +10,13 @@ import sys
 
 # When creating the sdist, make sure the django.mo file also exists:
 if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('fluent_comments')
     try:
-        os.chdir('fluent_comments')
-        # from django.core.management.commands.compilemessages import Command
-        # Command.compile_messages(sys.stderr)
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if 'sdist' in sys.argv:
+            raise
     finally:
         os.chdir('..')
 
