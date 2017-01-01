7c684dbe5b64691e2f7d2defa53cc9b5378da2fc
Diederik van der Boor <vdboor@edoburu.nl>
2017-01-01 22:12:32 +0100

    mention python/django versions in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+python -c "
+import sys, django, os
+sys.stderr.write('Using Python version {0} from {1}\n'.format(sys.version[:5], sys.executable))
+sys.stderr.write('Using Django version {0} from {1}\n'.format(
+    django.get_version(), os.path.dirname(os.path.abspath(django.__file__))))
+"
+
 exec example/manage.py test "$@"
