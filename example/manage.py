681e2606f1f036fe65aec4be226fa249725bb551
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-31 23:12:16 +0100

    Show warnings in travis and coverage

--- example/manage.py
+++ example/manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    # Same effect as python -Wd when run via coverage:
+    import warnings
+    warnings.simplefilter('always', DeprecationWarning)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
